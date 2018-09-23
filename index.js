ae44a58a82085ff08d964b96a8b69f9b55910138
carlo montanaro <carloldn@outlook.com>
2018-09-23 15:02:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var recipes = new Object ({})
 var obj = { prop: 1 }
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},obj,{[key]:value})}
-  function updateObjectWithKeyAndValue(object, key, value) {
-    return
-  }
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
